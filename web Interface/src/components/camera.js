0e669909f28651df2d63e25c5dd930fe5c8c6f8f
mayaan <ayaantherock1@gmail.com>
2022-02-04 20:05:17 +0530

    add image manipulation

--- web Interface/src/components/camera.js
+++ web Interface/src/components/camera.js
@@ -1,84 +1,168 @@
-import react from 'react'
+import { useState, useEffect } from "react";
+import * as fas from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './camera.js'
 import './camera.css'
-let constraint={
-            video:{
-                height:{max:1200},
-                width:{max:1200},
-                aspectRatio: { ideal:window.innerHeight > window.innerWidth? 1.4:1/1.4},
-                facingMode:"environment"
-            }
-        }
-export default class CameraPage extends react.Component{
-    constructor(props){
-        super(props)
-        this.handleCorrectShutter=this.handleCorrectShutter.bind(this)
-        this.handleWrongShutter=this.handleWrongShutter.bind(this)
-    }
-    componentDidMount(){
-        this.videoDoc = document.querySelector("#livePhoto video")
-        
-        try{
-        navigator.mediaDevices.getUserMedia(constraint).then(stream=>{
-            window.stream=stream;
-            this.videoDoc.srcObject=stream
-        }).catch(err=>console.log(err.message))
-    } catch{
-        console.log("media not supported on insecure devices")
+function startCamera() {
+    const video = document.getElementById("video")
+    let ratioInPortrait = video.clientHeight / video.clientWidth
+    let constraint = {
+        video: {
+            width: { max: 1200 },
+            aspectRatio: (window.innerHeight > window.innerWidth) ? ratioInPortrait : 1 / ratioInPortrait,
+            facingMode: 'environment'
         }
     }
+    navigator.mediaDevices.getUserMedia(constraint).then(stream => {
+        video.srcObject = stream;
+    })
+}
 
-    handleCorrectShutter(){
-        let videoDoc = document.querySelector("#livePhoto video")
-        let canvas=document.getElementById("cnv")
-        let img=document.getElementById('canvas');
-        const a=document.createElement("a")
-        canvas.width = videoDoc.width
-        canvas.height = videoDoc.height
-        canvas.getContext("2d").drawImage(videoDoc, 0, 0,videoDoc.height,videoDoc.width)
-        canvas.toBlob(blob=>{
-            img.src=canvas.toDataURL()
-            img.style.width = videoDoc.videoWidth+"px"
-            img.style.height = videoDoc.videoHeight+"px"
-            a.href=URL.createObjectURL(blob);
-            a.download="downlod.png"
-            //a.click();
-            videoDoc.parentElement.style.height="0"
-            videoDoc.srcObject.getTracks()[0].stop()
-        })
-    }
-    handleWrongShutter(){
-        try {
-            navigator.mediaDevices.getUserMedia(constraint).then(stream => {
-                window.stream = stream;
-                this.videoDoc.srcObject = stream
-                let img = document.getElementById('canvas');
-                let videoDoc = document.querySelector("#livePhoto video")
-                videoDoc.srcObject = window.stream
-                img.style.height = 0
-                videoDoc.parentElement.style.height = "70vh"
-            }).catch(err => console.log(err.message))
-        } catch {
-            console.log("media not supported on insecure devices")
+function Video(props) {
+    useEffect(() => {
+        if(props.state)
+        startCamera()
+        return () => {
         }
+    })
+    return (
+        <>
+            <video id="video" poster="logo192.png" style={styles.Video} autoPlay ></video>
+        </>
+    )
+}
 
+function ShutterButton(props) {
+    function handleClickTick(){
+        let video=document.getElementById('video')
+        let a= document.createElement('a');
+        a.style.display="none"
+        document.body.append(a)
+        a.href=video.poster
+        a.download="gridData.png"
+        a.click()
+        a.remove()
     }
-    render(){
-        return (
-            <>
-            <img id="canvas"></img>
-            <canvas id="cnv" height="800" width="800" style={{display:"none"}}></canvas>
-            <div id="livePhoto">
-              <video poster="logo192.png" width="100%" height="100%" autoPlay></video>
+    function handleClickCancel() {
+        props.stateChanger(1-props.state)
+    }
+    return (
+        <>
+            <div style={styles.ShutterButton}>
+                <p className="block tick" onClick={handleClickTick}> <FontAwesomeIcon icon={fas.faCheck} style={{ fontSize: "5rem", color: 'blue' }} /></p>
+                <p className="block cross" onClick={handleClickCancel}><FontAwesomeIcon icon={fas.faTimes} style={{ fontSize: "5rem" }} /></p>
             </div>
-              <div id="shutter">
-                <div className="correctShutter" onClick={this.handleCorrectShutter}>
-                        <span> &#10004;</span>
-                </div>
-                <div className="wrongShutter" onClick={this.handleWrongShutter}>
-                        <span> 	&#10060;</span>
-                </div>
-              </div>
-            </>
+        </>
+    );
+}
+
+
+function CapturePhoto(props) {
+    function handleTakePhoto() {
+        let canvas = document.getElementById('canvas')
+        let ctx = canvas.getContext("2d")
+        let video = document.getElementById('video')
+        let X = canvas.width = video.videoWidth;
+        let Y = canvas.height = video.videoHeight;
+        ctx.drawImage(video, 0, 0, X, Y, 0, 0, X, Y);
+        if (props.state){
+            video.srcObject.getTracks()[0].stop()
+            video.srcObject=null
+            video.poster=canvas.toDataURL()
+        }
+        props.stateChanger(1-props.state)
+    }
+    function handleCancelPhoto(){
+        props.exitDispatch(1)
+    }
+    return (
+        <div style={styles.ShutterButton}>
+            <div className="capture shot" onClick={handleTakePhoto}></div>
+            <p className="capture cancel" onClick={handleCancelPhoto}><FontAwesomeIcon icon={fas.faTimes} style={{ fontSize: "100%" }} /></p>
+        </div>
+    );
+}
+
+function Camera(props) {
+    let [voi, chVoi] = useState(1)
+    if(props.exitState)
+       return null;
+    let Comp;
+    if (voi){
+        Comp=(
+        <CapturePhoto exitDispatch={props.exitDispatch} stateChanger={chVoi} state={voi} />
         );
     }
+    else{
+    Comp=(
+        <ShutterButton stateChanger={chVoi} state={voi}/>
+    );
+    }
+    return (
+        <>
+        <div style={styles.VideoWrapper} id="VideoWrapper">
+            <Video state={voi}/>
+        </div>
+        {Comp}
+        </>
+    );
+}
+
+
+export default function Main(props) {
+    let [exit,exitDispatch]=useState(0)
+    if(exit){
+     return null;
+    }
+    else{
+    return (
+        <>
+            <div id="Main" style={styles.Main}>
+                <Camera exitDispatch={exitDispatch} exitState={exit}/>
+            </div>
+            <canvas id="canvas" width="100" height="100" style={{display:'none'}}></canvas>
+        </>
+    );
+    }
+}
+
+
+const styles = {
+    Main: {
+        border: "9px groove brown",
+        boxShadow: "0 0 3px 5px gray",
+        height: "100vh",
+        maxWidth: "400px",
+        boxSizing: "border-box",
+        borderRadius: "7px",
+        display: "flex",
+        flexFlow: "column nowrap",
+    },
+    ShutterButton: {
+        border: "2px solid black",
+        marginTop: 2,
+        display: 'grid',
+        backgroundColor: 'dodgerBlue',
+        gridTemplate: "100px / auto auto",
+        border: '2px solid red',
+        alignContent: 'center',
+        borderRadius: '10px'
+    },
+    Video: {
+        height: "100%",
+        width: "100%",
+        boxSizing: 'content-box'
+    },
+    VideoWrapper: {
+        position:"relative",
+        flex: 8.5,
+        width: '100%',
+        borderBottom: "2px solid #3f3f3f",
+        backgroundColor: 'gray',
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 8,
+        objectFit: "contain",
+        overflow: "hidden"
+    },
+
 }
